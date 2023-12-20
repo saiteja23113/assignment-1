f3f36c726caf50135e8c3ed7ac6046fcce96ab56
saiteja23113 <116273136+saiteja23113@users.noreply.github.com>
2023-12-20 14:18:20 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,154 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+     int n;
+     cin>>n;
+    // for (int i=1;i<=n;i++){
+    //     for(int j=1;j<=n;j++){
+    //         cout<<j<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+    
+    
+    // for (int i=1;i<=n;i++){
+    //     for(int j=1;j<=n-i+1;j++){
+    //         cout<<j<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // return 0;
+    
+    // for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=i;j++){
+    //         cout<<char(64+j)<<" ";
+    //     }
+    //     cout<<endl;
+    
+    
+    // for(int i=1;i<=n+1;i++){
+    //     for(int j=1;j<=i;j++){
+    //         if(i%2==0){
+    //             cout<<char(64+j)<<" ";
+                
+    //         }
+            
+    //         else cout<<j<<" ";
+    //     }
+    //     cout<<endl;
+    //}
+    
+    // for(int i=1;i<=n;i++){
+    //     //for spaces
+    //     for(int j=1;j<=n+1-i;j++){
+    //         cout<<" ";
+    //     }
+    //     for(int k=1;k<=i;k++){
+    //         cout<<char(64+k);
+    //     }
+    //     cout<<endl;
+    // }
+    
+    
+    // for(int i=1;i<=n;i++){
+    //     int k=i;
+    //     for(int j=1;j<=i;j++){
+            
+    //         cout<<k<<" ";
+    //         k--;
+    //     }
+        
+    //     cout<<endl;
+    // }
+    
+    // for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=n+1-i;j++){
+    //         cout<<" ";
+    //     }
+    //     for(int k=1;k<=n;k++){
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+    
+    
+    // int m;
+    // cin>>m;
+    // for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=m;j++){
+    //         if(i==1 || i==n || j==1 || j==m) cout<<"*";
+    //         else cout<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+    
+    
+    
+    // for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=i;j++){
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+        
+    // for(int i=1;i<=n-1;i++){
+    //     for(int j=1;j<=n-i;j++){
+    //         cout<<"*";
+            
+    //     }
+    //     cout<<endl;
+    // }
+    
+    
+    
+    // for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=n-i;j++){
+    //         cout<<" ";}
+        
+    //     for(int k=1;k<=i;k++){
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+        
+    // }
+    
+    // for(int i=1;i<=n-1;i++){
+    //     for(int j=1;j<=i;j++){
+    //         cout<<" ";
+    //     }
+        
+    //     for(int k=1;k<=n-i;k++){
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+    
+    // for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=n-i;j++){
+    //         cout<<" ";
+    //     }
+    //     for(int k=1;k<=2*i-1;k++){
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+        
+    // }
+    
+    
+    
+    
+    
+}
+
